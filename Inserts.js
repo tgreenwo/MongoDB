a53218ed64cb6d356b8628493541fa7e466f2845
tgreenwo <46994488+tgreenwo@users.noreply.github.com>
2019-01-24 12:36:43 +0100

    Add files via upload

--- Inserts.js
+++ Inserts.js
@@ -1,13 +1,10 @@
 use knowtech;
 
-for (var i=1; i < 10000; i++) 
-{
-db.alumno.insert(
-	{
-idAlumno: i ,
-Nombre: "Pepe" ,
-Apellidos: "Pepino" ,
-Dni: "X1112223"
-	}
-				)
-};
+for (var i = 1; i < 10000; i++) {
+	db.alumno.insertOne({
+		idAlumno: i ,
+		Nombre: "Pepe" ,
+		Apellidos: "Pepino" ,
+		Dni: "X1112223" +i
+	});
+}
